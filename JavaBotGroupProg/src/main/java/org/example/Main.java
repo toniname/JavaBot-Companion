42e3981a600db0d9cef5d37a4c95ea47977a802a
Alina <141162669+Alina-Triukhan@users.noreply.github.com>
2023-12-21 21:29:02 +0200

    Delete JavaBotGroupProg/src/main/java/org/example/Main.java
    d

--- JavaBotGroupProg/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.example.telegram.TelegramBotService;
-
-public class Main {
-    public static void main(String[] args) {
-        TelegramBotService tbs = new TelegramBotService();
-    }
-}
