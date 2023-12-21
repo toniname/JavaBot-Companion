ce3d1eea27465501917eb032350428da84ee8814
Alina <141162669+Alina-Triukhan@users.noreply.github.com>
2023-12-21 20:54:15 +0200

    Delete JavaBotGroupProg/src/main/java/org/example/currency directory
    same

--- JavaBotGroupProg/src/main/java/org/example/currency/Currency.java
+++
@@ -1,9 +0,0 @@
-package org.example.currency;
-
-public enum Currency {
-
-    USD,
-    EUR,
-    UAH
-
-}
--- JavaBotGroupProg/src/main/java/org/example/currency/CurrencyRatePrettier.java
+++
@@ -1,6 +0,0 @@
-package org.example.currency;
-
-public interface CurrencyRatePrettier {
-
-    String pretty(double rate, Currency ccy);
-}
