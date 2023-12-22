4052ba8c35f5ebcfad97aa8af341cbae0cefd2ec
master <alya26271@gmail.com>
2023-12-22 19:43:47 +0200

    test

---
+++ JavaBotGroupProg/src/main/java/org/example/telegram/command/Commands.java
@@ -0,0 +1,10 @@
+package org.example.telegram.command;
+
+public class Commands {
+    public static final String SETTINGS = "settings";
+    public static final String BANK = "bank";
+    public static final String USD = "usd";
+    public static final String MONO = "mono";
+    public static final String PRYVAT = "pryvat";
+
+}
