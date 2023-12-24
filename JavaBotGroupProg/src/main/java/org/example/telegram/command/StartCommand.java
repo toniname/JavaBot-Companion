9f32a1dc88b10078af376a7d0fcab415dc6c1cb1
Aomine7777 <141518740+Aomine7777@users.noreply.github.com>
2023-12-24 11:27:07 +0200

    add Picture

--- JavaBotGroupProg/src/main/java/org/example/telegram/command/StartCommand.java
+++ JavaBotGroupProg/src/main/java/org/example/telegram/command/StartCommand.java
@@ -4,13 +4,16 @@ import org.example.telegram.CurrencyTelegramBot;
 import org.example.telegram.userdata.SelectedOptions;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -53,12 +56,15 @@ public class StartCommand extends BotCommand {
             CurrencyTelegramBot.getUsersOptions().put(chat.getId(), newEntity);
         }
 
-
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chat.getId().toString());
+        sendPhoto.setPhoto(new InputFile(new File("JavaBotGroupProg/src/main/java/org/example/images/patron.png")));
         try {
+            absSender.execute(sendPhoto);
             absSender.execute(sm);
         } catch (TelegramApiException e) {
             System.out.println("Error");
         }
 
     }
-}
+    }
