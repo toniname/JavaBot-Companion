b618f31ed64c5a11ad187541bbba959eb672ba8e
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2023-12-24 02:54:47 +0100

    add token to gitignore

--- JavaBotGroupProg/src/main/java/org/example/telegram/LoginAndToken.java
+++
@@ -1,6 +0,0 @@
-package org.example.telegram;
-
-public class LoginAndToken {
-    public static final String NAME = "Patron_exchanger_bot";
-    public static final String TOKEN = "6731428914:AAHzFeVcvMVLZpy2AnQT9G8OXfSy1Z8GNR8";
-}
