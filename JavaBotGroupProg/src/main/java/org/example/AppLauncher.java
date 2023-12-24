b38b5cb18fba9e110130c81044bde8030dae55bd
toniname <60040991+toniname@users.noreply.github.com>
2023-12-24 17:33:38 +0200

    change Maine to App

---
+++ JavaBotGroupProg/src/main/java/org/example/AppLauncher.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.telegram.TelegramBotService;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        TelegramBotService tbs = new TelegramBotService();
+
+    }
+}
