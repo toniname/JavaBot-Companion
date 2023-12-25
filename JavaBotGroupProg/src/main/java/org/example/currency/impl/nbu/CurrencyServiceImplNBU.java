1c769081c2dc291ddd86d0da8be45dde15230251
Alina <alya26271@gmail.com>
2023-12-26 00:29:05 +0100

    clean

--- JavaBotGroupProg/src/main/java/org/example/currency/impl/nbu/CurrencyServiceImplNBU.java
+++ JavaBotGroupProg/src/main/java/org/example/currency/impl/nbu/CurrencyServiceImplNBU.java
@@ -53,32 +53,4 @@ public class CurrencyServiceImplNBU implements CurrencyService {
         allCurrencies = new Gson().fromJson(jsonString, type);
     }
 
-    public double getRate(Currency cc) {
-        String urlNBU = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json";
-
-        String jsonStringNBU = "";
-
-        try {
-
-            jsonStringNBU = Jsoup.connect(urlNBU)
-                    .ignoreContentType(true)
-                    .get()
-                    .body()
-                    .text();
-
-        } catch (IOException e) {
-            System.out.println("Error while currency request: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        Type typeNBU = TypeToken.getParameterized(List.class, CurrencyItemDtoNBU.class)
-                .getType();
-
-        List<CurrencyItemDtoNBU> listNBU = new Gson().fromJson(jsonStringNBU, typeNBU);
-        return listNBU.stream()
-                .filter(c -> c.getCc() == cc)
-                .map(CurrencyItemDtoNBU::getRate)
-                .findFirst()
-                .orElseThrow();
-    }
 }
